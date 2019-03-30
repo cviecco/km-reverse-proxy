4327825261e3431e508e285a7e161bb99ef4fb58
Camilo Viecco <cviecco@gmail.com>
2019-03-29 19:17:44 -0700

    simple-test-1

--- cmd/km-reverse-proxy/authz_handler_test.go
+++ cmd/km-reverse-proxy/authz_handler_test.go
@@ -26,3 +26,32 @@ func TestGetUserGroupsOSSuccess(t *testing.T) {
 		t.Fatal(err)
 	}
 }
+
+func TestGetUserGroupsSuccess(t *testing.T) {
+	user, err := user.Current()
+	if err != nil {
+		t.Fatal(err)
+	}
+	h := AuthZHandler{
+		//handler:        handler,
+		//configLocation: configLocation,
+		userInfo: make(map[string]UserInfoCacheEntry),
+		//ldapConfig:     ldapConfig,
+	}
+	_, err = h.getUserGroups(user.Username)
+	if err != nil {
+		t.Fatal(err)
+	}
+	//ensure cache exits
+	validEntry, ok := h.userInfo[user.Username]
+	if !ok {
+		t.Fatal(err)
+	}
+	//force a cache entry and test
+	const testCacheOnlyUsername = "foobarbaz"
+	h.userInfo[testCacheOnlyUsername] = validEntry
+	_, err = h.getUserGroups(testCacheOnlyUsername)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
