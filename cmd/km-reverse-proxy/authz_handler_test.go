fc78ea656a67250b37e9ed432c9ea01050252400
Camilo Viecco <cviecco@gmail.com>
2019-04-01 21:08:29 -0700

    adding more tests and fixing authz handler for longest prefix match

--- cmd/km-reverse-proxy/authz_handler_test.go
+++ cmd/km-reverse-proxy/authz_handler_test.go
@@ -3,12 +3,47 @@ package main
 import (
 	//"errors"
 	//"fmt"
-	//"net/http"
+	"io/ioutil"
+	"net/http"
 	//"net/http/httptest"
 	//"net/url"
+	"os"
 	"os/user"
 	"testing"
 )
+
+const testConfigFileContent = `#comment
+protected_paths:
+  - path: "/"
+    allowed_groups: ["group1", "group2"]
+  - path: "/foobar"
+    allowed_groups: ["group1", "group3"]
+`
+
+func TestLoadConfig(t *testing.T) {
+	tmpfile, err := ioutil.TempFile("", "example")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.Remove(tmpfile.Name()) // clean up
+
+	if _, err := tmpfile.Write([]byte(testConfigFileContent)); err != nil {
+		t.Fatal(err)
+	}
+	if err := tmpfile.Close(); err != nil {
+		t.Fatal(err)
+	}
+	h := AuthZHandler{
+		//handler:        handler,
+		configLocation: tmpfile.Name(),
+		//userInfo: make(map[string]UserInfoCacheEntry),
+		//ldapConfig:     ldapConfig,
+	}
+	err = h.LoadConfig()
+	if err != nil {
+		t.Fatal(err)
+	}
+}
 
 func TestGetUserGroupsOSSuccess(t *testing.T) {
 	user, err := user.Current()
@@ -54,4 +89,63 @@ func TestGetUserGroupsSuccess(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
+}
+
+func TestCanUserAccessRequest(t *testing.T) {
+	user, err := user.Current()
+	if err != nil {
+		t.Fatal(err)
+	}
+	h := AuthZHandler{
+		//handler:        handler,
+		//configLocation: configLocation,
+		userInfo: make(map[string]UserInfoCacheEntry),
+		//ldapConfig:     ldapConfig,
+	}
+	userGroups, err := h.getUserGroups(user.Username)
+	if err != nil {
+		t.Fatal(err)
+	}
+	h.currentConfig = &AuthZConfiguration{
+		ProtectedPaths: []ProtectedPath{
+			ProtectedPath{
+				Path:          "/protected",
+				AllowedGroups: []string{"supercoolGroup"},
+			},
+			ProtectedPath{
+				Path:          "/",
+				AllowedGroups: userGroups,
+			},
+		},
+	}
+	req, err := http.NewRequest("GET", "/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	ok, err := h.canUserAccessRequest(user.Username, req)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !ok {
+		t.Fatal("should have succeeded")
+	}
+	ok, err = h.canUserAccessRequest("randomuser", req)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if ok {
+		t.Fatal("should have failed")
+	}
+	req2, err := http.NewRequest("GET", "/protected/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	ok, err = h.canUserAccessRequest(user.Username, req2)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if ok {
+		t.Fatal("should have failed")
+	}
+
 }
